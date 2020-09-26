b268f0725cd22c6317bc7412005f1b685d7f645b
Barne Jürgensen <barne.juergensen@codeanker.de>
2020-09-26 09:42:13 +0200

    add prfile data

--- pages/profile/_type/index.vue
+++ pages/profile/_type/index.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Test dd</h1>
+  <b-container>
+    <pre>{{ profiles }}</pre>
+  </b-container>
 </template>
+
+<script>
+import { createClient } from '~/plugins/contentful.js'
+const client = createClient()
+
+export default {
+  async asyncData ({ env, params: { type } }) {
+    const { items: profiles } = await client.getEntries({
+      content_type: 'profile',
+      order: 'fields.name',
+      'fields.type': type
+    })
+
+    return {
+      profiles
+    }
+  }
+}
+</script>
