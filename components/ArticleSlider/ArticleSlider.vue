81b77308e85a1bb27ee79c25c07ba6c32e44119f
Alexander Ingwersen <alexander.ingwersen@53north.digital>
2020-09-26 13:24:07 +0200

    add slick slider package and ArticleSlider

---
+++ components/ArticleSlider/ArticleSlider.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="article-slider">
+    <VueSlickCarousel v-bind="settings">
+      <div class="article-slider__item">
+        <img src="https://via.placeholder.com/1080x400" alt="Image" class="article-slider__item-media">
+        <div class="article-slider__item-caption">
+          <h3 class="article-slider__item-title">
+            Headline 3
+          </h3>
+        </div>
+      </div>
+      <div class="article-slider__item">
+        <img src="https://via.placeholder.com/1080x400" alt="Image" class="article-slider__item-media">
+        <div class="article-slider__item-caption">
+          <h3 class="article-slider__item-title">
+            Headline 3
+          </h3>
+        </div>
+      </div>
+      <div class="article-slider__item">
+        <img src="https://via.placeholder.com/1080x400" alt="Image" class="article-slider__item-media">
+        <div class="article-slider__item-caption">
+          <h3 class="article-slider__item-title">
+            Headline 3
+          </h3>
+        </div>
+      </div>
+    </VueSlickCarousel>
+  </div>
+</template>
+
+<style lang="scss">
+  @import "./ArticleSlider.scss";
+  @import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+</style>
+
+<script>
+import VueSlickCarousel from 'vue-slick-carousel'
+
+export default {
+  components: { VueSlickCarousel },
+  data () {
+    return {
+      settings: {
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              dots: false
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: true,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              arrows: false,
+              dots: true
+            }
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <b-container>
+    <ArticleSlider />
     <ArticleTeaser />
     <pre>{{ posts }}</pre>
   </b-container>
