21711cf1181ad65487d2f6affd4c57002339cecf
Barne Jürgensen <barne.juergensen@codeanker.de>
2020-09-26 02:04:34 +0200

    add infinity scroll

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,46 @@
 <template>
   <b-container>
     <ArticleTeaser v-for="post in posts" :key="post.sys.id" :post="post" />
+    <infinite-loading @infinite="infiniteHandler" />
   </b-container>
 </template>
 
 <script>
 import { createClient } from '~/plugins/contentful.js'
 const client = createClient()
-
+const limit = 3
 export default {
   async asyncData ({ env }) {
     const { items: posts } = await client.getEntries({
       content_type: 'post',
+      limit,
       order: '-sys.createdAt'
     })
 
     return {
       posts
+    }
+  },
+  data () {
+    return {
+      skip: 0
+    }
+  },
+  methods: {
+    async infiniteHandler ($state) {
+      this.skip += limit
+      const { items: posts } = await client.getEntries({
+        content_type: 'post',
+        limit,
+        skip: this.skip,
+        order: '-sys.createdAt'
+      })
+      if (posts.length) {
+        this.posts.push(...posts)
+        $state.loaded()
+      } else {
+        $state.complete()
+      }
     }
   }
 }
