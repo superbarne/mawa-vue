b8f95b61a97e900413f2f13cf653b75522e162e2
Barne Jürgensen <barne.juergensen@codeanker.de>
2020-09-26 09:57:07 +0200

    add category switch

--- pages/profile/_type/category/_slug.vue
+++ pages/profile/_type/category/_slug.vue
@@ -14,19 +14,27 @@ import { createClient } from '~/plugins/contentful.js'
 const client = createClient()
 
 export default {
-  async asyncData ({ env, params: { type } }) {
-    const { items: profiles } = await client.getEntries({
-      content_type: 'profile',
-      order: 'fields.name',
-      'fields.type': type
+  async asyncData ({ env, params: { type, slug } }) {
+    const { items: [category] } = await client.getEntries({
+      content_type: 'category',
+      'fields.slug[in]': slug
     })
+
     const { items: categories } = await client.getEntries({
       content_type: 'category',
       'fields.type': type
     })
 
+    const { items: profiles } = await client.getEntries({
+      content_type: 'profile',
+      order: 'fields.name',
+      'fields.type': type,
+      'fields.categories.sys.id': category.sys.id
+    })
+
     return {
       profiles,
+      category,
       categories,
       type
     }
